bda832e736f97dfb61e6bfc55b8bd869bcea070b
Peter L Gunter Jr <petlgunjr@gmail.com>
2020-01-30 15:53:11 -0800

    Geocodio response from azure function

--- afterwardzchromescraperazure/__init__.py
+++ afterwardzchromescraperazure/__init__.py
@@ -1,14 +1,17 @@
 import logging
 import json
+from geocodio import GeocodioClient
 
 import azure.functions as func
 
+client = GeocodioClient("fb531bb1bc80c53e5f0a88b5ff5efc30fbebc15")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
   logging.info('Python HTTP trigger function processed a request.')
   try:
     req_body = req.get_json()
-    return json.dumps(req_body)
+    location = client.geocode(req_body["Location"])
+    return json.dumps(location)
   except ValueError:
     return func.HttpResponse(
       "Could not parse json",
