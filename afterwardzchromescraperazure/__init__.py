dee914ef54863072a03b445fbc11dc8d630cbf14
Peter L Gunter Jr <petlgunjr@gmail.com>
2020-01-30 11:41:01 -0800

    fix return

--- afterwardzchromescraperazure/__init__.py
+++ afterwardzchromescraperazure/__init__.py
@@ -7,7 +7,8 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
   logging.info('Python HTTP trigger function processed a request.')
   try:
-    return req.get_json()
+    req_body = req.get_json()
+    return json.dumps(req_body)
   except ValueError:
     return func.HttpResponse(
       "Could not parse json",
