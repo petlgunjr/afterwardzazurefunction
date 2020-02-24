9f921902746c46aaefc6c33ba2fd71f9a6ff3719
Peter L Gunter Jr <petlgunjr@gmail.com>
2020-02-24 09:26:53 -0800

    Commit for computer swap

--- afterwardzchromescraperazure/__init__.py
+++ afterwardzchromescraperazure/__init__.py
@@ -1,8 +1,14 @@
 import logging
 import json
 from geocodio import GeocodioClient
-
 import azure.functions as func
+import pyodbc
+
+conn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=wardzwall.database.windows.net.aftwall;DATABASE=afterwardz;UID=apiTEST;PWD=Te$This1OuttatHere;")
+
+cursor = conn.cursor()
+results = cursor.execute("SELECT location FROM Geodata")
+logging.info(results)
 
 client = GeocodioClient("fb531bb1bc80c53e5f0a88b5ff5efc30fbebc15")
 
@@ -10,10 +16,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
   logging.info('Python HTTP trigger function processed a request.')
   try:
     req_body = req.get_json()
-    location = client.geocode(req_body["Location"], fields=["school"])
-    return json.dumps(location)
+    geocodioRequest = client.geocode(req_body["websiteAddress"], fields=["school"])
+    # melissaRequest = await melissa.verify_address(req_body["websiteLocation"])
+    geocodioResponse = json.dumps(geocodioRequest)
+    # melissaResponse = json.dumps(melissaRequest)
+    logging.info(geocodioResponse)
+    return (geocodioResponse)
   except ValueError:
     return func.HttpResponse(
       "Could not parse json",
       status_code=400
-    )
+)
