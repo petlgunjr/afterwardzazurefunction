1ed867a63582ab8b5a9c16caca8b64e60ea057c3
Peter L Gunter Jr <petlgunjr@gmail.com>
2020-03-06 08:36:18 -0800

    sync computers

--- __init__.py
+++ __init__.py
@@ -9,21 +9,3 @@ conn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=wardzwall.d
 cursor = conn.cursor()
 results = cursor.execute("SELECT location FROM Geodata")
 logging.info(results)
-
-client = GeocodioClient("fb531bb1bc80c53e5f0a88b5ff5efc30fbebc15")
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-  logging.info('Python HTTP trigger function processed a request.')
-  try:
-    req_body = req.get_json()
-    geocodioRequest = client.geocode(req_body["websiteAddress"], fields=["school"])
-    # melissaRequest = await melissa.verify_address(req_body["websiteLocation"])
-    geocodioResponse = json.dumps(geocodioRequest)
-    # melissaResponse = json.dumps(melissaRequest)
-    logging.info(geocodioResponse)
-    return (geocodioResponse)
-  except ValueError:
-    return func.HttpResponse(
-      "Could not parse json",
-      status_code=400
-  )
